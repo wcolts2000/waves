4f472621630012d92f011fb27657ff2adee9524d
wcolts2000 <wcolts2000@gmail.com>
2019-05-02 15:13:07 -0700

    added brands and woods get and post and auth logout funcitonality

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const helmet = require("helmet");
 require("dotenv").config();
 
 const app = express();
@@ -10,6 +11,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
 
 app.use(
+  helmet(),
   express.json(),
   bodyParser.urlencoded({ extended: true }),
   cookieParser()
@@ -17,9 +19,58 @@ app.use(
 
 // Models
 const { User } = require("./models/user");
+const { Brand } = require("./models/brand");
+const { Wood } = require("./models/wood");
 
 // Middlewares
 const { auth } = require("./middleware/auth");
+const { admin } = require("./middleware/admin");
+
+// ====================
+//        WOODS
+// ====================
+
+app.post("/api/product/wood", auth, admin, (req, res) => {
+  const wood = new Wood(req.body);
+
+  wood.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(201).json({
+      success: true,
+      wood: doc
+    });
+  });
+});
+
+app.get("/api/product/woods", (req, res) => {
+  Wood.find({}, (err, woods) => {
+    if (err) return res.status(404).send(err);
+    res.status(200).send(woods);
+  });
+});
+
+// ====================
+//        BRAND
+// ====================
+
+app.post("/api/product/brand", auth, admin, (req, res) => {
+  const brand = new Brand(req.body);
+
+  brand.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(201).json({
+      success: true,
+      brand: doc
+    });
+  });
+});
+
+app.get("/api/product/brands", (req, res) => {
+  Brand.find({}, (err, brands) => {
+    if (err) return res.status(404).send(err);
+    res.status(200).send(brands);
+  });
+});
 
 // ====================
 //        USERS
@@ -43,7 +94,7 @@ app.post("/api/users/register", (req, res) => {
 
   user.save((err, doc) => {
     if (err) return res.json({ success: false, err });
-    res.status(200).json({ success: true, userdata: doc });
+    res.status(201).json({ success: true, userdata: doc });
   });
 });
 
@@ -75,7 +126,17 @@ app.post("/api/users/login", (req, res) => {
   });
 });
 
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true
+    });
+  });
+});
+
 const port = process.env.port || 8081;
 
 app.listen(port, () => {
-  console.log(`\n === Server Listening On Port ${port} === \n`)});
+  console.log(`\n === Server Listening On Port ${port} === \n`);
+});
